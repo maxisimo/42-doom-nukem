ae9ce01290f5581caf1440de3a23279b74526f9d
Leo CHAPPON <lchappon@e1r7p16.42.fr>
2018-12-18 15:12:26 +0100

    better enemies walls collisions

--- enemies.c
+++ enemies.c
@@ -6,7 +6,7 @@
 /*   By: maxisimo <maxisimo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/13 18:12:53 by maxisimo          #+#    #+#             */
-/*   Updated: 2018/12/18 14:56:31 by lchappon         ###   ########.fr       */
+/*   Updated: 2018/12/18 15:12:08 by lchappon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -78,16 +78,16 @@ void		enemies_init(t_app *a)
 void		enemies_ai(t_app *a, t_coord_d *pos)
 {
 	if (a->pos.x > pos->x &&
-			a->map[(int)(pos->x + 0.26)][(int)(pos->y + 0.25)] == 0)
+			a->map[(int)(pos->x + 0.25)][(int)(pos->y)] == 0)
 		pos->x += 0.01;
 	else if (a->pos.x < pos->x &&
-			a->map[(int)(pos->x - 0.26)][(int)(pos->y + 0.25)] == 0)
+			a->map[(int)(pos->x - 0.25)][(int)(pos->y)] == 0)
 		pos->x -= 0.01;
 	if (a->pos.y > pos->y &&
-			a->map[(int)(pos->x + 0.25)][(int)(pos->y + 0.26)] == 0)
+			a->map[(int)(pos->x)][(int)(pos->y + 0.25)] == 0)
 		pos->y += 0.01;
 	else if (a->pos.y < pos->y &&
-			a->map[(int)(pos->x + 0.25)][(int)(pos->y - 0.26)] == 0)
+			a->map[(int)(pos->x)][(int)(pos->y - 0.25)] == 0)
 		pos->y -= 0.01;
 }
 
