bba3721f8479314bd4268fc819ea24183cc8bc4b
maxisimo <34480775+maxisimo@users.noreply.github.com>
2018-12-19 20:47:21 +0100

    Update main.c

--- map_generator/main.c
+++ map_generator/main.c
@@ -19,9 +19,8 @@ int		main(int ac, char **av)
 	if (ac == 4 && ft_strisnumeric(av[2]) == 1 && ft_strisnumeric(av[3]) == 1)
 	{
 		argc4(av[1], av[2], av[3], &map);
-		mlx_win_init(&map);
-		draw_grill(0, 0, &map);
 		init_tab(&map);
+		mlx_win_init(&map);
 		mlx_hook(map.win, 2, (1L << 0), &key_hook, &map);
 		mlx_mouse_hook(map.win, mouse_hook, &map);
 		mlx_loop(map.mlx);
@@ -31,8 +30,6 @@ int		main(int ac, char **av)
 		argc2(av[1], &map);
 		init(&map);
 		mlx_win_init(&map);
-		draw_grill(0, 0, &map);
-		put_color2(0, 0, &map);
 		mlx_hook(map.win, 2, (1L << 0), &key_hook, &map);
 		mlx_mouse_hook(map.win, mouse_hook, &map);
 		mlx_loop(map.mlx);
