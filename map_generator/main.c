05a9507f8a6281ae0618ebd788f4773472e78dab
Leo CHAPPON <lchappon@e1r5p1.42.fr>
2019-01-02 15:48:32 +0100

    * removed

--- map_generator/main.c
+++ map_generator/main.c
@@ -6,7 +6,7 @@
 /*   By: maxisimo <maxisimo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/29 19:39:00 by maxisimo          #+#    #+#             */
-/*   Updated: 2018/12/18 16:00:46 by maxisimo         ###   ########.fr       */
+/*   Updated: 2019/01/02 15:48:06 by lchappon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ int		main(int ac, char **av)
 	if (ac == 4 && ft_strisnumeric(av[2]) == 1 && ft_strisnumeric(av[3]) == 1)
 	{
 		is_argc_4(av[1], av[2], av[3], &map);
-*		init_tab(&map);
+		init_tab(&map);
 		start(&map);
 		mlx_hook(map.win, 2, (1L << 0), &key_hook, &map);
 		mlx_mouse_hook(map.win, mouse_hook, &map);
