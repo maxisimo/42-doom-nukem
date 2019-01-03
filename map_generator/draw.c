d9437472886fc3f67d7ac6bd4249b320f7ac6146
Maxime SIMON <maxisimo@e1r12p18.42.fr>
2019-01-03 19:34:25 +0100

    modified mapg

--- map_generator/draw.c
+++ map_generator/draw.c
@@ -6,7 +6,7 @@
 /*   By: maxisimo <maxisimo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/05 12:16:44 by maxisimo          #+#    #+#             */
-/*   Updated: 2019/01/02 16:39:00 by lchappon         ###   ########.fr       */
+/*   Updated: 2019/01/03 19:06:29 by maxisimo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,10 +50,23 @@ static void	ft_put_bmp(t_map *map, t_bmp bmp, int x, int y)
 
 static void	choose_color(int x, int y, t_map *map)
 {
-	if (map->map[map->y][map->x] < 0)
-		put_px_to_img(map, x, y, 0xFF4242);
-	else if (map->map[map->y][map->x] == 0)
-		put_px_to_img(map, x, y, 0);
+	int		clr;
+	
+	clr = (map->map[map->y][map->x] == 0) ? 0 : 0xFF4242;
+	if (map->map[map->y][map->x] <= 0)
+	{
+		map->tempx = x;
+		while (map->tempx < x + map->blocw - 1)
+		{
+			map->tempy = y;
+			while (map->tempy < y + map->bloch - 1)
+			{
+				put_px_to_img(map, map->tempx, map->tempy, clr);
+				map->tempy++;
+			}
+			map->tempx++;
+		}
+	}
 	else
 		ft_put_bmp(map, map->textures[map->i - 1], x, y);
 }
@@ -61,26 +74,34 @@ static void	choose_color(int x, int y, t_map *map)
 
 void		infos(t_map *map)
 {
-	map->textures_scale = 3.125;
-	mlx_string_put(map->mlx, map->win, 1350, 100, 0xFFFFFF, "Press tab to change texture");
-	mlx_string_put(map->mlx, map->win, 1350, 200, 0xFFFFFF, "Press 'esc' or 's' to save and quit");
-	mlx_string_put(map->mlx, map->win, 1350, 300, 0xFFFFFF, "Press 'P' to activate/desactivate the player position");
-	mlx_string_put(map->mlx, map->win, 1350, 400, 0xFFFFFF, "Press the left mouse button to put a wall / player position");
-	mlx_string_put(map->mlx, map->win, 1350, 500, 0xFFFFFF, "Press the right mouse button to take off a wall / player position");
-	ft_put_bmp(map, map->textures[map->i - 1], 1380, 600);
+	//map->textures[map->i - 1].scale = 3.125;
+	mlx_string_put(map->mlx, map->win, 1310, 100, 0xFFFFFF, "Press 'tab' to change texture");
+	mlx_string_put(map->mlx, map->win, 1310, 200, 0xFFFFFF, "Press 'esc' or 's' to save and quit");
+	mlx_string_put(map->mlx, map->win, 1310, 300, 0xFFFFFF, "Press 'P' to activate/desactivate the player position");
+	mlx_string_put(map->mlx, map->win, 1310, 400, 0xFFFFFF, "Press the left mouse button to put a wall / player position");
+	mlx_string_put(map->mlx, map->win, 1310, 500, 0xFFFFFF, "Press the right mouse button to take off a wall / player position");
+	ft_put_bmp(map, map->textures[map->i - 1], 1310, 600);
 }
 
 
 void		put_color2(int x, int y, t_map *map)
 {
+	map->temp2x = 0;
 	while (x < map->width)
 	{
+		y = 0;
+		map->temp2y = 0;
 		while (y < map->height)
 		{
+			map->x = x / map->blocw;
+			map->y = y / map->bloch;
+			map->i = map->map[map->temp2y][map->temp2x];
 	        choose_color(x, y, map);
 			y += map->bloch;
+			map->temp2y++;
 		}
 		x += map->blocw;
+		map->temp2x++;
 	}
 }
 
@@ -90,7 +111,7 @@ void		put_color(int x, int y, t_map *map)
 	map->y = y / map->bloch;
 	map->tempx = (map->x == 0) ? 1 : map->x * map->blocw;
     map->tempy = (map->y == 0) ? 0 : map->y * map->bloch;
-	fill_tab(map->tempx, map->tempy, map->i, map);
+	fill_tab(map->x, map->y, map->i, map);
 	choose_color(map->tempx, map->tempy, map);
 }
 
@@ -99,17 +120,16 @@ void		draw_map(int x, int y, t_map *map)
 	int		color;
 
 	color = 0xFFFFFF;
-	map->textures_scale = 64 / map->blocw;
 	while (x < map->width)
 	{
-		y = map->height - 1;
-		while (y >= 0)
+		y = 0;
+		while (y < map->height)
 		{
-			if (x == 0 || y == map->height - 1)
+			if (x == 0 || y == 0)
 				put_px_to_img(map, x, y, color);
 			if (((x + 1) % map->blocw) == 0 || ((y + 1) % map->bloch) == 0)
 				put_px_to_img(map, x, y, color);
-			y--;
+			y++;
 		}
 		x++;
 	}
--- map_generator/utils.c
+++ map_generator/utils.c
@@ -6,7 +6,7 @@
 /*   By: maxisimo <maxisimo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/05 11:54:43 by maxisimo          #+#    #+#             */
-/*   Updated: 2019/01/02 16:38:21 by lchappon         ###   ########.fr       */
+/*   Updated: 2019/01/03 19:32:38 by maxisimo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,13 @@
 
 static void	texture_load(t_map *map)
 {
-	bmp_loadfile(&map->textures[0], "textures/bigdoor.bmp");
-	bmp_loadfile(&map->textures[1], "textures/dude.bmp");
-	bmp_loadfile(&map->textures[2], "textures/metal.bmp");
-	bmp_loadfile(&map->textures[3], "textures/floor.bmp");
-	bmp_loadfile(&map->textures[4], "textures/skulwall.bmp");
-	bmp_loadfile(&map->textures[5], "textures/marbface.bmp");
-	bmp_loadfile(&map->textures[6], "textures/skulwall2.bmp");
+	bmp_loadfile(&map->textures[0], "../textures/bigdoor.bmp");
+	bmp_loadfile(&map->textures[1], "../textures/dude.bmp");
+	bmp_loadfile(&map->textures[2], "../textures/metal.bmp");
+	bmp_loadfile(&map->textures[3], "../textures/floor.bmp");
+	bmp_loadfile(&map->textures[4], "../textures/skulwall.bmp");
+	bmp_loadfile(&map->textures[5], "../textures/marbface.bmp");
+	bmp_loadfile(&map->textures[6], "../textures/skulwall2.bmp");
 }
 
 void		error(void)
@@ -41,10 +41,10 @@ void		is_argc_2(char *s, t_map *map)
 void		is_argc_4(char *s1, char *s2, char *s3, t_map *map)
 {
 	map->ac = 4;
-	map->width = (abs(ft_atoi(s2)) > 55) ? 55 : abs(ft_atoi(s2));
-	map->height = (abs(ft_atoi(s3)) > 55) ? 55 : abs(ft_atoi(s3));
-	map->blocw = 1320 / map->width;
-	map->bloch = 1320 / map->height;
+	map->width = (abs(ft_atoi(s2)) > 40) ? 40 : abs(ft_atoi(s2));
+	map->height = (abs(ft_atoi(s3)) > 40) ? 40 : abs(ft_atoi(s3));
+	map->blocw = 1280 / map->width;
+	map->bloch = 1280 / map->height;
 	map->width *= map->blocw;
 	map->height *= map->bloch;
 	map->name = s1;
@@ -55,8 +55,8 @@ void		is_argc_4(char *s1, char *s2, char *s3, t_map *map)
 void		start(t_map *map)
 {
 	map->mlx = mlx_init();
-	map->win = mlx_new_window(map->mlx, 1320 + 700, 1320, map->name);
-	map->img = mlx_new_image(map->mlx, 1320, 1320);
+	map->win = mlx_new_window(map->mlx, 1280 + 700, 1280, map->name);
+	map->img = mlx_new_image(map->mlx, 1280, 1280);
 	map->img_ptr = mlx_get_data_addr(map->img,
 			&map->bpp, &map->sl, &map->endian);
 	texture_load(map);
