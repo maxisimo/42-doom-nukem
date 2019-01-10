3e6e2269997aa953a992e62ae27870acb1b2817c
Leo CHAPPON <lchappon@e1r5p21.42.fr>
2019-01-10 18:24:30 +0100

    skull as final item

--- load.c
+++ load.c
@@ -6,7 +6,7 @@
 /*   By: maxisimo <maxisimo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/16 11:54:23 by thbernar          #+#    #+#             */
-/*   Updated: 2019/01/07 19:42:59 by maxisimo         ###   ########.fr       */
+/*   Updated: 2019/01/10 18:20:57 by lchappon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,10 +34,10 @@ void	sprites_load(t_app *a)
 	bmp_loadfile(&a->sprites[0], "sprites/zombie1.bmp");
 	bmp_loadfile(&a->sprites[1], "sprites/barril.bmp");
 	bmp_loadfile(&a->sprites[2], "sprites/pillar.bmp");
-	bmp_loadfile(&a->sprites[3], "sprites/piece1.bmp");
-	bmp_loadfile(&a->sprites[4], "sprites/piece2.bmp");
-	bmp_loadfile(&a->sprites[5], "sprites/piece2.bmp");
-	bmp_loadfile(&a->sprites[6], "sprites/piece1.bmp");
+	bmp_loadfile(&a->sprites[3], "sprites/skull1.bmp");
+	bmp_loadfile(&a->sprites[4], "sprites/skull2.bmp");
+	bmp_loadfile(&a->sprites[5], "sprites/skull3.bmp");
+	bmp_loadfile(&a->sprites[6], "sprites/skull4.bmp");
 	bmp_loadfile(&a->weapon.img, "sprites/ak47.bmp");
 	bmp_loadfile(&a->weapon.img2, "sprites/ak47fired.bmp");
 	bmp_loadfile(&a->weapon.scope, "sprites/scope.bmp");
